a9ca060551f164af1b2f94538d4e7981a96e609a
maxomm <max.warsinke@googlemail.com>
2022-06-28 14:29:50 +0200

    change

--- EDI-Test/tests/test_edi_test.py
+++ EDI-Test/tests/test_edi_test.py
@@ -1,11 +1,6 @@
 import time
 
-from edi_test import __version__
 from src.edi_test.new_main import time_milliseconds
-
-
-def test_version():
-    assert __version__ == "0.1.0"
 
 
 def test_time_milliseconds():
