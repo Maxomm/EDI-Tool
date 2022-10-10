79354a2efa2790caf9b7217fa1cca5884cef1968
maxomm <max.warsinke@googlemail.com>
2022-10-10 21:08:04 +0200

    move file

--- Tool/tests/test_edi_test.py
+++ Tool/tests/test_edi_test.py
@@ -4,7 +4,7 @@ from src.main import EmotionController, resource_path
 
 def test_emotion():
     emo_controller = EmotionController()
-    test_img = cv2.imread(resource_path("Tool/src/angry_test.jpg"))
+    test_img = cv2.imread(resource_path("Tool/tests/angry_test.jpg"))
     emotion = emo_controller.get_emotion_from_image(test_img)
     print(emotion)
     assert emotion == "Angry"
